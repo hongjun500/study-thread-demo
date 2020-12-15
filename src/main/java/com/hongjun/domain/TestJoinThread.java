aae652ca3c55c440f5e1cb95b9480cb7e7a99b8e
hongjun500 <bhj.hong@qq.com>
2020-12-15 22:07:16 +0800

    join方法补充

--- src/main/java/com/hongjun/domain/TestJoinThread.java
+++ src/main/java/com/hongjun/domain/TestJoinThread.java
@@ -19,6 +19,13 @@ public class TestJoinThread {
             // main线程通过join方法等待t线程结束后再继续执行打印 最后end，那么最终打印结果将是这个 1先start 2再hello 3最后end
             // 这个join方法被注释后最后的打印结果将是 1 先start,2 最后end,3 再hello
             t.join();
+            // join方法源码:如下
+            /**
+             * public final void join() throws InterruptedException {
+             *                 join(0);
+             *             }
+             */
+            // 如上可以看出还有一个重载方法，入参表示毫秒数，代表这个等待线程结束的时间（和sleep方法有出入），超过了等待时间就不再继续等待
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
